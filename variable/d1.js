34db178d7caf825947735c0ad86b5ddd08e9d6cb
ojhankit <ojha27ankit@gmail.com>
2024-03-22 00:27:13 -0700

    basic syntax

---
+++ variable/d1.js
@@ -0,0 +1,25 @@
+// variable and constant
+
+// console.log("Ankit")
+// constant matlab ek bar initialize hogya to fir change nahi hoga
+
+const accountId = 12345
+let accountEmail = "xyz@gmail.com"
+var accountPassword = "12345"
+//accountCity = "Jaipur"
+
+let accountState;
+// accountId = 2 not allowed!!
+
+accountEmail = "ank@gmail.com"
+accountPassword = "123233"
+accountCity = "kolkata"
+// console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+// var use nahi hota itna zyada
+/*
+    {
+        ye curly braces scope hota hai
+    }
+*/
