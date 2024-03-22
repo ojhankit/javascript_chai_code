9cba6b8b74e39ff82bfb8b17f734194672e49ee2
ojhankit <ojha27ankit@gmail.com>
2024-03-22 00:30:27 -0700

    eee

--- variable/d2.js
+++ variable/d2.js
@@ -1,3 +1,23 @@
 "use strict"; //treat all js code as newer version
 
-alert("hello")
+// alert("hello")  we are using nodeJs
+
+//console.log(3+2); console.log("ankit") // readability dekho 
+
+
+let name = "Ankit"
+let age = 20
+let isLogin = true
+let state = null
+
+console.log(state);
+// number => 2^53
+//bigInt 
+//string
+//boolean
+//null standalone
+
+//symbol => uniqueness
+// object
+
+console.log(typeof(null))
