1891b83d491dbf7b86b5fdace5aed43a3a3b9acc
ojhankit <ojha27ankit@gmail.com>
2024-03-24 13:40:01 -0700

    data type conversion

---
+++ variable/d3.js
@@ -0,0 +1,21 @@
+// datatype  conversion
+
+//let score = "33abx"
+// const {score} = req.body
+//console.log(typeof(score)) // number
+
+//let valueInNumber = Number(score) // it will gurantee that score is number 
+// 
+
+//33 => 33
+// "33ab" => nan
+
+// let login="ankit"
+// let boollogin = Boolean(login)
+// console.log(boollogin)
+
+let num = 33
+let string_num = String(num)
+console.log(string_num);
+console.log(typeof string_num);
+console.log(typeof num);
